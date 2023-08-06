73853e0395424b7e381a2b6347a911093cc2f83c
Mayberry <2673220823@qq.com>
2023-08-06 23:24:18 +0800

    搭建WeBottomBar

--- app/src/main/java/com/mayberry/wecompose/MainActivity.kt
+++ app/src/main/java/com/mayberry/wecompose/MainActivity.kt
@@ -3,41 +3,43 @@ package com.mayberry.wecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.mayberry.wecompose.ui.theme.WeBottomBar
 import com.mayberry.wecompose.ui.theme.WeComposeTheme
 
 class MainActivity : ComponentActivity() {
+    var selectedTab by mutableStateOf(0);
+    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WeComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Column {
+                    val viewModel: WeViewModel = viewModel()
+                    HorizontalPager(count = 4) { page ->
+                        when(page) {
+                            0 -> Box(Modifier.fillMaxSize())
+                            1 -> Box(Modifier.fillMaxSize())
+                            2 -> Box(Modifier.fillMaxSize())
+                            3 -> Box(Modifier.fillMaxSize())
+                        }
+                    }
+                    WeBottomBar(viewModel.selectedTab) {
+                        viewModel.selectedTab = it
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeComposeTheme {
-        Greeting("Android")
-    }
 }
